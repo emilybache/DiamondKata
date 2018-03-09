a4b5dd01f18e157fee3a781997c01aa3fc951091
emilybache <emily.bache@praqma.com>
2018-03-09 14:52:03 +0100

    added new version of this kata which should drive a different implementation

---
+++ python/test_diamond_centrist_incremental.py
@@ -0,0 +1,111 @@
+"""
+These test cases can be used to test-drive a solution to the diamond kata, in an incremental manner.
+
+to run the tests, use 'py.test' - see http://pytest.org
+
+Instructions:
+1. Make the first test case for Diamond A pass
+2. change the 'ignore_' to 'test_' for the next test case. Make it pass.
+3. Uncomment the next line of the test case. Make it pass
+4. When all the lines of code in the test case are passing, continue to the next test case.
+5. When all the test cases in this file are uncommented and passing, you should have a full working solution.
+""" 
+
+from diamond import *
+
+def test_A_Diamond():
+    assert "A\n" == diamond("A")
+
+def ignore_test_size():
+    assert size("A") == 1
+    #assert size("B") == 3
+    #assert size("C") == 5
+    #assert size("D") == 7
+
+def ignore_test_diamond_spaces():
+    assert diamond_spaces("A") == [[" "]]
+    #assert diamond_spaces("B") == [[" ", " ", " "],
+    #                               [" ", " ", " "],
+    #                               [" ", " ", " "]]
+    #assert diamond_spaces("C") == [[" ", " ", " ", " ", " "],
+    #                               [" ", " ", " ", " ", " "],
+    #                               [" ", " ", " ", " ", " "],
+    #                               [" ", " ", " ", " ", " "],
+    #                               [" ", " ", " ", " ", " "]]
+    #assert diamond_spaces("D") == [[" ", " ", " ", " ", " ", " ", " "],
+    #                               [" ", " ", " ", " ", " ", " ", " "],
+    #                               [" ", " ", " ", " ", " ", " ", " "],
+    #                               [" ", " ", " ", " ", " ", " ", " "],
+    #                               [" ", " ", " ", " ", " ", " ", " "],
+    #                               [" ", " ", " ", " ", " ", " ", " "],
+    #                               [" ", " ", " ", " ", " ", " ", " "]]
+
+def ignore_test_center_coordinate():
+    assert (0, 0) == center_coordinate("A")
+    # assert (1, 1) == center_coordinate("B")
+    # assert (2, 2) == center_coordinate("C")
+    # assert (3, 3) == center_coordinate("D")
+
+def ignore_test_place_letter_center():
+    assert [["A"]] == place_letter("A", {(0, 0)}, middle_letter="A", spaces=diamond_spaces("A"))
+    # assert [[" ", " ", " "],
+    #         [" ", "A", " "],
+    #         [" ", " ", " "]] == place_letter("A", {(0, 0)}, middle_letter="B", spaces=diamond_spaces("B"))
+    # assert [[" ", " ", " ", " ", " "],
+    #         [" ", " ", " ", " ", " "],
+    #         [" ", " ", "A", " ", " "],
+    #         [" ", " ", " ", " ", " "],
+    #         [" ", " ", " ", " ", " "]] == place_letter("A", {(0, 0)}, middle_letter="C", spaces=diamond_spaces("C"))
+
+def ignore_test_place_letter_off_center():
+    assert [["A", " ", " "],
+            [" ", " ", " "],
+            [" ", " ", " "]] == place_letter("A", {(-1, 1)}, middle_letter="B", spaces=diamond_spaces("B"))
+    # assert [[" ", " ", " "],
+    #         [" ", " ", " "],
+    #         ["A", " ", " "]] == place_letter("A", {(-1, -1)}, middle_letter="B", spaces=diamond_spaces("B"))
+    # assert [[" ", "A", " "],
+    #         [" ", " ", " "],
+    #         [" ", "A", " "]] == place_letter("A", {(0, 1), (0, -1)}, middle_letter="B", spaces=diamond_spaces("B"))
+    # assert [[" ", " ", " "],
+    #         ["A", " ", "A"],
+    #         [" ", " ", " "]] == place_letter("A", {(-1, 0), (1, 0)}, middle_letter="B", spaces=diamond_spaces("B"))
+    
+def ignore_test_coordinates_of_A():
+    assert {(0, 0)} == coordinates("A", middle_letter="A")
+    # assert {(0, 1), (0, -1)} == coordinates("A", middle_letter="B")
+    # assert {(0, 2), (0, -2)} == coordinates("A", middle_letter="C")
+    # assert {(0, 3), (0, -3)} == coordinates("A", middle_letter="D")
+
+def ignore_test_coordinates_middle_letter():
+    assert {(0, 0)} == coordinates("A", middle_letter="A")
+    # assert {(-1, 0), (1, 0)} == coordinates("B", middle_letter="B")
+    # assert {(-2, 0), (2, 0)} == coordinates("C", middle_letter="C")
+    # assert {(-3, 0), (3, 0)} == coordinates("D", middle_letter="D")
+
+def ignore_test_coordinates_of_B():
+    assert {(-1, 1), (-1, -1), (1, 1), (1, -1)} == coordinates("B", middle_letter="C")
+    # assert {(-1, 2), (-1, -2), (1, 2), (1, -2)} == coordinates("B", middle_letter="D")
+    # assert {} == coordinates("B", middle_letter="A")
+
+def ignore_test_coordinates_of_C():
+    assert {(-2, 1), (-2, -1), (2, 1), (2, -1)} == coordinates("C", middle_letter="D")
+    #assert {} == coordinates("C", middle_letter="A")
+
+def ignore_DiamondC():
+    assert diamond.Diamond('C').print_diamond() == """\
+  A
+ B B
+C   C
+ B B
+  A"""
+
+def ignore_DiamondD():
+    assert diamond.Diamond('D').print_diamond() == """\
+   A
+  B B
+ C   C
+D     D
+ C   C
+  B B
+   A"""
---
+++ python/test_diamond_centrist_iterative.py
@@ -0,0 +1,149 @@
+"""
+These test cases can be used to test-drive a solution to the diamond kata, in an interative manner.
+The idea is that you iterate towards a full solution, each test cycle you are closer to a full solution 
+than in the previous one. The thing with iterating is you may delete stuff that was there before,
+or add stuff you know you will need to delete later.
+
+When you have got a test to pass, you will 'recycle' it, ie hide/delete the previous one.
+This is counter-intuitive for many people!
+
+to run the tests, use 'py.test' - see http://pytest.org
+
+Instructions:
+1. Make the first test for Diamond A (which is failing) pass
+2. change the 'ignore_' to 'test_' in the next test case. Make it pass too.
+3. Uncomment the next line of the test case. Make it pass
+4. When you've got a new test case passing, you may find
+you need to COMMENT OUT A PREVIOUS TEST that now fails. 
+This is expected. You are 'recycling' tests.
+5. When all the 'DiamondX' test cases in this file are uncommented and passing, 
+you should have a full working solution.
+
+"""
+import diamond
+
+ 
+def test_DiamondA():
+    assert diamond.Diamond('A').print_diamond() == "A"
+
+def ignore_Diamond_with_only_spaces():
+    assert diamond.Diamond('A').diamond() == [[" "]]
+    # assert diamond.Diamond('B').diamond() == \
+    #     [[" ", " ", " "],
+    #      [" ", " ", " "],
+    #      [" ", " ", " "]]
+    # assert diamond.Diamond('C').diamond() == \
+    #     [[" ", " ", " ", " ", " "],
+    #      [" ", " ", " ", " ", " "],
+    #      [" ", " ", " ", " ", " "],
+    #      [" ", " ", " ", " ", " "],
+    #      [" ", " ", " ", " ", " "]]
+
+def ignore_Diamond_with_center_marked_with_a_Z():
+    assert diamond.Diamond('A').diamond() == [["Z"]]
+    # assert diamond.Diamond('B').diamond() == \
+    #     [[" ", " ", " "],
+    #      [" ", "Z", " "],
+    #      [" ", " ", " "]]
+    # assert diamond.Diamond('C').diamond() == \
+    #     [[" ", " ", " ", " ", " "],
+    #      [" ", " ", " ", " ", " "],
+    #      [" ", " ", "Z", " ", " "],
+    #      [" ", " ", " ", " ", " "],
+    #      [" ", " ", " ", " ", " "]]
+
+def ignore_Diamond_with_1_0_coordinates_marked_with_a_Z():
+    assert diamond.Diamond('A').diamond() == [[" "]]
+    # assert diamond.Diamond('B').diamond() == \
+    #     [[" ", " ", " "],
+    #      [" ", " ", "Z"],
+    #      [" ", " ", " "]]
+
+def ignore_Diamond_with_0_1_coordinates_marked_with_a_Z():
+    assert diamond.Diamond('B').diamond() == \
+        [[" ", "Z", " "],
+         [" ", " ", " "],
+         [" ", " ", " "]]
+    # assert diamond.Diamond('C').diamond() == \
+    #     [[" ", " ", " ", " ", " "],
+    #      [" ", " ", "Z", " ", " "],
+    #      [" ", " ", " ", " ", " "],
+    #      [" ", " ", " ", " ", " "],
+    #      [" ", " ", " ", " ", " "]]
+     
+def ignore_Diamond_with_minus2_1_coordinates_marked_with_a_Z():
+    assert diamond.Diamond('C').diamond() == \
+        [[" ", " ", " ", " ", " "],
+         ["Z", " ", " ", " ", " "],
+         [" ", " ", " ", " ", " "],
+         [" ", " ", " ", " ", " "],
+         [" ", " ", " ", " ", " "]]
+
+def ignore_Diamond_plot_As():
+    assert diamond.Diamond('B').diamond() == \
+        [[" ", "A", " "],
+         [" ", " ", " "],
+         [" ", "A", " "]]
+    # assert diamond.Diamond('C').diamond() == \
+    #     [[" ", " ", "A", " ", " "],
+    #      [" ", " ", " ", " ", " "],
+    #      [" ", " ", " ", " ", " "],
+    #      [" ", " ", " ", " ", " "],
+    #      [" ", " ", "A", " ", " "]]
+
+def ignore_Diamond_plot_As_and_middle_letter():
+    assert diamond.Diamond('B') == \
+        [[" ", "A", " "],
+         ["B", " ", "B"],
+         [" ", "A", " "]]
+    # assert diamond.Diamond('C') == \
+    #     [[" ", " ", "A", " ", " "],
+    #      [" ", " ", " ", " ", " "],
+    #      ["C", " ", " ", " ", "C"],
+    #      [" ", " ", " ", " ", " "],
+    #      [" ", " ", "A", " ", " "]]
+    # assert diamond.Diamond('D').diamond() == \
+    #     [[" ", " ", " ", "A", " ", " ", " "],
+    #      [" ", " ", " ", " ", " ", " ", " "],
+    #      [" ", " ", " ", " ", " ", " ", " "],
+    #      ["D", " ", " ", " ", " ", " ", "D"],
+    #      [" ", " ", " ", " ", " ", " ", " "],
+    #      [" ", " ", " ", " ", " ", " ", " "],
+    #      [" ", " ", " ", "A", " ", " ", " "]]
+
+def ignore_DiamondB():
+    assert diamond.Diamond('B').print_diamond() == " A\nB B\n A"
+ 
+def ignore_Diamond_plot_other_letter():
+    assert diamond.Diamond('C').diamond() == \
+        [[" ", " ", "A", " ", " "],
+         [" ", "B", " ", "B", " "],
+         ["C", " ", " ", " ", "C"],
+         [" ", "B", " ", "B", " "],
+         [" ", " ", "A", " ", " "]]
+    # assert diamond.Diamond('D').diamond() == \
+    #     [[" ", " ", " ", "A", " ", " ", " "],
+    #      [" ", " ", "B", " ", "B", " ", " "],
+    #      [" ", "C", " ", " ", " ", "C", " "],
+    #      ["D", " ", " ", " ", " ", " ", "D"],
+    #      [" ", "C", " ", " ", " ", "C", " "],
+    #      [" ", " ", "B", " ", "B", " ", " "],
+    #      [" ", " ", " ", "A", " ", " ", " "]]
+
+def ignore_DiamondC():
+    assert diamond.Diamond('C').print_diamond() == """\
+  A
+ B B
+C   C
+ B B
+  A"""
+
+def ignore_DiamondD():
+    assert diamond.Diamond('D').print_diamond() == """\
+   A
+  B B
+ C   C
+D     D
+ C   C
+  B B
+   A"""
