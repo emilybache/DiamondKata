a1dd85c68d9aa4887bd9604c58c62753c476ce8f
Anis CHAABANI <chaabani.anis@gmail.com>
2020-02-17 14:40:20 +0100

    Php version centrist incremental test

---
+++ php/src/Point.php
@@ -0,0 +1,24 @@
+<?php
+
+
+class Point
+{
+    private $x;
+    private $y;
+
+    public function __construct(int $x, int $y)
+    {
+        $this->x = $x;
+        $this->y = $y;
+    }
+
+    public function getX(): int
+    {
+        return $this->x;
+    }
+
+    public function getY(): int
+    {
+        return $this->y;
+    }
+}
